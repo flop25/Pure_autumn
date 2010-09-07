449305683519677f320c97d7616b5fe9a6000eb1
flop25 <flop25@piwigo.org>
2010-09-07 11:27:57 +0000

    version 1.0.0 for pwg 2.1.x
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_autumn@6859 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Theme Name: Pure_autumn
+Version: 1.0.0
+Description: Colors of autumn, random sort of colors for thumbnails, "S" blocks... a must have !
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=443
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
+$themeconf = array(
+  'name'         => 'Pure_autumn',
+  'parent'        => 'Pure_default',
+  'icon_dir'      => 'themes/Pure_default/icon',
+  'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+);
+
+?>
