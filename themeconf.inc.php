4b562e4015faee69682cb9e151a82f345d152a27
flop25 <flop25@piwigo.org>
2011-04-06 12:11:30 +0000

    no more tpl file : use of prefilter
    git-svn-id: http://piwigo.org/svn/extensions/floPure/Pure_autumn@10104 68402e56-0260-453c-a942-63ccdbb3a9ee

--- themeconf.inc.php
+++ themeconf.inc.php
@@ -13,6 +13,43 @@ $themeconf = array(
   'parent'        => 'Pure_default',
   'icon_dir'      => 'themes/Pure_default/icon',
   'mime_icon_dir' => 'themes/Pure_default/icon/mimetypes/',
+  'local_head' => 'local_head.tpl',
 );
+/** mainpage_categories.tpl **/
+add_event_handler('loc_end_index_category_thumbnails', 'Pure_autumn_cat');
+function Pure_autumn_cat($tpl_thumbnails_var)
+{
+    global $template;
+    $template->set_prefilter('index_category_thumbnails', 'Pure_autumn_prefilter_cat');
+		return $tpl_thumbnails_var;
+}
+function Pure_autumn_prefilter_cat($content, &$smarty)
+{
+  $search = '#<li>[\r\t\n]*<div class="thumbnailCategory">#';
+  
+  $replacement = '<li class="{cycle values="cat_1,cat_2,cat_3,cat_4"}" >
+	<div class="thumbnailCategory">';
 
+  return preg_replace($search, $replacement, $content);
+}
+
+/** thumbnails.tpl **/
+add_event_handler('loc_end_index_thumbnails', 'Pure_autumn_thumbnails');
+function Pure_autumn_thumbnails($tpl_thumbnails_var)
+{
+    global $template;
+    $template->set_prefilter('index_thumbnails', 'Pure_autumn_prefilter_thumbnails');
+    $template->set_prefilter('stuffs', 'Pure_autumn_prefilter_thumbnails');
+		return $tpl_thumbnails_var;
+}
+
+function Pure_autumn_prefilter_thumbnails($content, &$smarty)
+{
+  $search = '#<li>[\r\t\n]*<span class="wrap1">#';
+  
+  $replacement = '<li class="thumb_{"1"|mt_rand:5}"  >
+	<span class="wrap1">';
+
+  return preg_replace($search, $replacement, $content);
+}
 ?>
